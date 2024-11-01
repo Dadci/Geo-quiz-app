0ccf3493349c0df26b1d5fb8827d48dca97afec6
Raouf <dadci.r@gmail.com>
2024-11-01 01:30:45 +0100

    added useful comments

--- src/components/Quizlayout.jsx
+++ src/components/Quizlayout.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
 
 const Quizlayout = ({ questions }) => {
-    const [currentQuestion, setCurrentQuestion] = useState(0)
-    const [optionChosen, setOptionChosen] = useState('')
-    const [score, setScore] = useState(0)
-    const [showScore, setShowScore] = useState(false)
+    const [currentQuestion, setCurrentQuestion] = useState(0) // to keep track of the current question
+    const [optionChosen, setOptionChosen] = useState('') // to keep track of the option chosen by the user
+    const [score, setScore] = useState(0) // to keep track of the score
+    const [showScore, setShowScore] = useState(false) // to keep track of whether to show the score or not
 
-    const { question, options, answer } = questions[currentQuestion]
+    const { question, options, answer } = questions[currentQuestion] // destructuring the question, options, and answer from the questions array
 
 
     return (
